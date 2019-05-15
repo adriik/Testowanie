26540c68ce597b84ca47e03e8f8805da147a25c9
adriik <adriik5@gmail.com>
2019-05-15 13:18:06 +0200

    Update Testowa.java

--- Testowa.java
+++ Testowa.java
@@ -5,6 +5,10 @@ public class Testowa {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("192.168.0.1");
+		System.out.println("192.168.0.2:6000");
+		
+		System.out.println("google.pl");
+		System.out.println("C:\\Users\\adrii\\.p2\\pool\\features");
 	}
 
 }
