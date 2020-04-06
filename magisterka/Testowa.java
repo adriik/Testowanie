3232b705fb845fbcff1edb2e78788bc5f45b407e
adriik <adriik5@gmail.com>
2020-04-06 15:29:49 +0200

    delete comment

--- magisterka/Testowa.java
+++ magisterka/Testowa.java
@@ -3,7 +3,7 @@ package magisterka;
 public class Testowa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		System.out.println("192.168.0.1");
 		System.out.println("192.168.0.2:6000");
 		
